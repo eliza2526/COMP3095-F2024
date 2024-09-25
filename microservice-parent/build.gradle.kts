410919671b3ed6ed4be196194aad93bd9bebd8b1
Elizabeth Thomas <elizabeth.thomas2@georgebrown.ca>
2024-09-24 21:20:31 -0400

    COMP3095 - First Commit

--- microservice-parent/build.gradle.kts
+++ microservice-parent/build.gradle.kts
@@ -19,7 +19,15 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers:mongodb")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
