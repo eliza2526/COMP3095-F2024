83f4bba5130953a442f9dbcd732d494f4775e713
Elizabeth Thomas <elizabeth.thomas2@georgebrown.ca>
2024-11-03 21:46:13 -0500

    COMP3095 - Commit

---
+++ microservice-parent/order-service/src/main/java/ca/gbc/orderservice/model/Order.java
@@ -0,0 +1,29 @@
+package ca.gbc.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNumber;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+
+
+
+}
