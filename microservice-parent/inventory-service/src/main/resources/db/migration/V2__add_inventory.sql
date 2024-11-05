97af93027450beca0685cde821835ad8084c00b2
Elizabeth Thomas <elizabeth.thomas2@georgebrown.ca>
2024-11-05 18:53:59 -0500

    COMP3095 - Commit order-service, inventory service working

---
+++ microservice-parent/inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,8 @@
+INSERT INTO t_inventory(quantity, sku_code)
+VALUES
+    (100, 'SKU00001'),
+    (200, 'SKU00002'),
+    (300, 'SKU00003'),
+    (400, 'SKU00004'),
+    (500, 'SKU00005'),
+    (600, 'SKU00005');
