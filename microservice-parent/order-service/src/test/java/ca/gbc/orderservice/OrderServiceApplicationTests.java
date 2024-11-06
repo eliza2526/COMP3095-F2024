653e6757b62a2629b7fb4d3d5d5872e2e1524c96
Elizabeth Thomas <elizabeth.thomas2@georgebrown.ca>
2024-11-05 21:46:42 -0500

    COMP3095 - Commit Nov 5, 2024

--- microservice-parent/order-service/src/test/java/ca/gbc/orderservice/OrderServiceApplicationTests.java
+++ microservice-parent/order-service/src/test/java/ca/gbc/orderservice/OrderServiceApplicationTests.java
@@ -1,15 +1,59 @@
 package ca.gbc.orderservice;
 
+import ca.gbc.orderservice.client.InventoryClient;
+import ca.gbc.orderservice.stub.InventoryClientStub;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.PostgreSQLContainer;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
+    @ServiceConnection
+    static PostgreSQLContainer<?> postgresSQLContainer = new PostgreSQLContainer<>("postgres:15-apline");
+
+    private Integer port;
+
+    void setUp(){
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        postgresSQLContainer.start();
+    }
+
+    void shouldSubmitOrder() {
+        String submitOrderJson = """
+                {
+                "skuCode" : "samsung_tv_2024"
+                "price" : 5000,
+                "quantity" : 10
+                }                
+                """;
+
+        //mock a call to inventory-service
+        InventoryClientStub.stubInventoryCall("samsung_tv_2024", 10);
+
+        var responseBodyString = RestAssured.given()
+                .contentType("application/json")
+                .body(submitOrderJson)
+                .when()
+                .post("/order")
+                .then()
+                .statusCode(201)
+                .extract()
+                .body().asString();
+
+        assertThat(responseBodyString, Matchers.is("Order Placed Successfully"));
+
     }
 
 }
